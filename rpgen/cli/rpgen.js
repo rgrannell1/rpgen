86cfbd95bb108e148040a03b5ef9d26999be76fa
rgrannell1 <r.grannell2@gmail.com>
2020-07-04 19:35:32 +0100

    improved documentation and added default

--- rpgen/cli/rpgen.js
+++ rpgen/cli/rpgen.js
@@ -9,23 +9,26 @@ Name:
   rpgen — Generate a high entropy, memorable password.
 
 Usage:
-  rpgen (-f WORDS | --fpath WORDS)     [-n NUM | --num NUM] [-p | --password-only] [-d DELIMITER | --delimiter DELIMITER]
-  rpgen (-g NAME  | --dictionary NAME) [-n NUM | --num NUM] [-p | --password-only] [-d DELIMITER | --delimiter DELIMITER]
+  rpgen
+  rpgen (-f WORDS | --fpath WORDS)     [-n NUM | --num NUM] [-p | --password-only] [-d DELIM | --delimiter DELIM]
+  rpgen (-g NAME  | --dictionary NAME) [-n NUM | --num NUM] [-p | --password-only] [-d DELIM | --delimiter DELIM]
   rpgen (-h | --help | --version)
 
 Description:
-  Rpgen — Random password generator — chooses random words from an input dictionary to use as a password. The program outputs three values, spaced out:
+  Rpgen — Random password generator — chooses random words from an input dictionary to use as a password. The program outputs three space-seperated values:
 
   * a password created by combining randomly-chosen words from a dictionary.
-  * the entropy of n words chosed from a dictionary of k words
-  * the entropy of the total number of letters chosen from a uniform character set of letters present in the dictionary. Note that the letter frequency will vary in most
-    dictionaries, so this is only a rough estimate.
+  * the naive entropy of a password of n words chosed from a dictionary of k words
+  * the entropy of the total number of letters chosen from a uniform character set of letters present in the dictionary. Note that the
+    letter frequency will vary in most dictionaries, so this is only a rough estimate.
 
-  The latter two values are useful guides to determining how hard a password will be to crack through brute-force attempts.
+  The latter two values are guides to help you determine how resistant a password is to brute-force guessing.
 
 Security:
   Random words are selected using a cryptographically secure random number generator derived from the Node.js builtin 'crypto.randomBytes'. This RNG
   is verified against the 'dieharder' test suite.
+
+  The strength of the password depends on the quality of the input dictionary. If a short or repetitive dictionary is chosen, generated passwords will be weak.
 
 Example:
   rpgen --dictionary english
@@ -33,10 +36,10 @@ Example:
 
 Options:
   -f WORDS, --fpath WORDS                A path containing the words to derive the password from.
-  -g NAME, --dictionary NAME             The name of the build-in dictionary to use. The supported dictionaries are:
+  -g NAME, --dictionary NAME             The name of the build-in dictionary to use. [default: english] The supported dictionaries are:
 ${dictionaries}
-  -d DELIMITER, --delimiter DELIMITER    The delimiter to use between words [default: -].
-  -n NUM, --num NUM                      The number of words to choose. [default: 6]
+  -d DELIM, --delimiter DELIM            The delimiter seperating words in the output password [default: -].
+  -n NUM, --num NUM                      The number of words to choose from the input file or dictionary. [default: 6]
   -p, --password-only                    Do not display the password entropy.
   -h, --help                             Display this documentation.
 
